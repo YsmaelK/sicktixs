93fd0e1d2d282ab37147748929c8cae90232586a
Ysmael <ysmaelkamgaing@gmail.com>
2024-03-11 01:42:30 -0700

    3/12/s2

--- index.js
+++ index.js
@@ -1,40 +1,34 @@
-import express from 'express';
-import fetch from 'node-fetch';
-import dotenv from 'dotenv';
-import path from 'path';
-import cors from 'cors';
-import nodemailer from 'nodemailer';
-
-dotenv.config();
-
+import express from "express";
+import fetch from "node-fetch";
+import "dotenv/config";
+import path from "path";
+import cors from "cors"
 const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET, PORT = 8888 } = process.env;
-const base = 'https://api-m.sandbox.paypal.com';
+const base = "https://api-m.sandbox.paypal.com";
 const app = express();
 
-// Configure CORS middleware
-app.use(cors({
-  origin: 'https://www.sicktixs.com', // Allow requests from this origin
-}));
+// host static files
+//app.use(express.static("client"));
+app.use(cors())
 
+// parse post params sent in body in json format
 app.use(express.json());
 
-// Configure nodemailer transporter
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD
-  }
-});
-
+/**
+ * Generate an OAuth 2.0 access token for authenticating with PayPal REST APIs.
+ * @see https://developer.paypal.com/api/rest/authentication/
+ */
 const generateAccessToken = async () => {
   try {
+    if (!PAYPAL_CLIENT_ID || !PAYPAL_CLIENT_SECRET) {
+      throw new Error("MISSING_API_CREDENTIALS");
+    }
     const auth = Buffer.from(
-      `${PAYPAL_CLIENT_ID}:${PAYPAL_CLIENT_SECRET}`
-    ).toString('base64');
+      PAYPAL_CLIENT_ID + ":" + PAYPAL_CLIENT_SECRET,
+    ).toString("base64");
     const response = await fetch(`${base}/v1/oauth2/token`, {
-      method: 'POST',
-      body: 'grant_type=client_credentials',
+      method: "POST",
+      body: "grant_type=client_credentials",
       headers: {
         Authorization: `Basic ${auth}`,
       },
@@ -43,19 +37,30 @@ const generateAccessToken = async () => {
     const data = await response.json();
     return data.access_token;
   } catch (error) {
-    console.error('Failed to generate Access Token:', error);
+    console.error("Failed to generate Access Token:", error);
   }
 };
 
+/**
+ * Create an order to start the transaction.
+ * @see https://developer.paypal.com/docs/api/orders/v2/#orders_create
+ */
 const createOrder = async (data) => {
+  console.log("Data sent to createOrder:", data);
+  // use the cart information passed from the front-end to calculate the purchase unit details
+  console.log(
+    "shopping cart information passed from the frontend createOrder() callback:",
+    data,
+  );
+
   const accessToken = await generateAccessToken();
   const url = `${base}/v2/checkout/orders`;
   const payload = {
-    intent: 'CAPTURE',
+    intent: "CAPTURE",
     purchase_units: [
       {
         amount: {
-          currency_code: 'USD',
+          currency_code: "USD",
           value: data.product.cost,
         },
       },
@@ -64,100 +69,85 @@ const createOrder = async (data) => {
 
   const response = await fetch(url, {
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       Authorization: `Bearer ${accessToken}`,
+
     },
-    method: 'POST',
+    method: "POST",
     body: JSON.stringify(payload),
   });
 
   return handleResponse(response);
 };
 
+/**
+ * Capture payment for the created order to complete the transaction.
+ * @see https://developer.paypal.com/docs/api/orders/v2/#orders_capture
+ */
 const captureOrder = async (orderID) => {
   const accessToken = await generateAccessToken();
   const url = `${base}/v2/checkout/orders/${orderID}/capture`;
 
   const response = await fetch(url, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       Authorization: `Bearer ${accessToken}`,
+      // Uncomment one of these to force an error for negative testing (in sandbox mode only). Documentation:
+      // https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
+      // "PayPal-Mock-Response": '{"mock_application_codes": "INSTRUMENT_DECLINED"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "TRANSACTION_REFUSED"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
     },
   });
 
   return handleResponse(response);
 };
 
-const handleResponse = async (response) => {
+async function handleResponse(response) {
   try {
-    const responseBody = await response.text();
-    const jsonResponse = JSON.parse(responseBody);
+    const responseBody = await response.text(); // Read the body as text
+    const jsonResponse = JSON.parse(responseBody); // Parse the JSON
     return {
       jsonResponse,
       httpStatusCode: response.status,
     };
   } catch (err) {
-    console.error('Failed to parse JSON response:', err);
+    console.error("Failed to parse JSON response:", err);
     return {
       jsonResponse: null,
       httpStatusCode: response.status,
-      error: 'Failed to parse JSON response',
+      error: "Failed to parse JSON response",
     };
   }
-};
+}
 
-app.post('/api/orders', async (req, res) => {
+app.post("/api/orders", async (req, res) => {
   try {
+    // use the cart information passed from the front-end to calculate the order amount detals
     const { cart } = req.body;
     const { jsonResponse, httpStatusCode } = await createOrder(cart);
     res.status(httpStatusCode).json(jsonResponse);
   } catch (error) {
-    console.error('Failed to create order:', error);
-    res.status(500).json({ error: 'Failed to create order.' });
+    console.error("Failed to create order:", error);
+    res.status(500).json({ error: "Failed to create order." });
   }
 });
 
-app.post('/api/orders/:orderID/capture', async (req, res) => {
+app.post("/api/orders/:orderID/capture", async (req, res) => {
   try {
     const { orderID } = req.params;
     const { jsonResponse, httpStatusCode } = await captureOrder(orderID);
     res.status(httpStatusCode).json(jsonResponse);
   } catch (error) {
-    console.error('Failed to create order:', error);
-    res.status(500).json({ error: 'Failed to capture order.' });
+    console.error("Failed to create order:", error);
+    res.status(500).json({ error: "Failed to capture order." });
   }
 });
 
 // serve index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('./client/checkout.html'));
-});
-
-app.post('/submit-form', async (req, res) => {
-  try {
-    const { firstName, lastName, email } = req.body;
-
-    const mailOptions = {
-      from: process.env.EMAIL_USER,
-      to: process.env.RECIPIENT_EMAIL,
-      subject: 'New Form Submission',
-      text: `New form submission received:\n\nFirst Name: ${firstName}\nLast Name: ${lastName}\nEmail: ${email}`
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error('Error sending email:', error);
-        res.status(500).json({ error: 'Failed to send email' });
-      } else {
-        console.log('Email sent:', info.response);
-        res.status(200).json({ message: 'Email sent successfully' });
-      }
-    });
-  } catch (error) {
-    console.error('Error submitting form:', error);
-    res.status(500).json({ error: 'Failed to submit form' });
-  }
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./client/checkout.html"));
 });
 
 app.listen(PORT, '0.0.0.0', () => {
